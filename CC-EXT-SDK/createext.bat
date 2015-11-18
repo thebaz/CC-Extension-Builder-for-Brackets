5e87b264e630cf6ed8f150d96ae0dcf5ad16c3a0
thebaz <luca.bassini@emerasoft.com>
2015-11-18 17:02:14 +0100

    SDK folder removal
    It does not make sense to have this folder inside this project

--- CC-EXT-SDK/createext.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-set sdk=%~dp0
-set dest=%APPDATA%\Adobe\CEP\extensions\%2
-XCOPY "%sdk%\templates\%1" %dest% /D /E /C /R /I /K /Y >nul
-echo %dest%
--- CC-EXT-SDK/deployext.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-set dest=%APPDATA%\Adobe\CEP\extensions\%2
-XCOPY "%1" %dest% /D /E /C /R /I /K /Y >nul
-echo %dest%
